836137b8f45a7cd56c36d372f9a49cfad8a6ec9e
RaviChandraMadipadiga <chanduravi64@gmail.com>
2021-07-03 02:38:29 +0530

    Second Commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 
 def index():
-    return "<h1>Hi My name is ravi chandra and this flask app demo and deployment through heroku</h1>"
+    return "<h1>Hi My name is ravi chandra and this flask app demo and deployment through heroku succesful</h1>"
 
 if __name__== '__main__':
     app.run()
